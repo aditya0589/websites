9f52e3b6408c4a92b30b878fedd17fc499033879
aditya0589 <144033691+aditya0589@users.noreply.github.com>
2024-11-02 19:56:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+document.getElementById('web-development').addEventListener('click', function() {
+    window.location.href = 'web-development.html'; 
+});
+
+
+document.getElementById('machine-learning').addEventListener('click', function() {
+    window.location.href = 'machine-learning.html'; 
+});
+
+
+document.getElementById('data-analytics').addEventListener('click', function() {
+    window.location.href = 'data-analytics.html'; 
+});
+
+document.getElementById('mail').addEventListener('click', function() {
+    window.location.href = "mailto:example@example.com"; 
+});
+
